2f4a10fd2380d03c09f2b078e56a18122f2a3d88
awais <mawaisnow@gmail.com>
2020-02-13 03:37:35 +0500

    changed few things about welcome page and addd user role to user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,7 +21,7 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->string('businessName')->nullable();
             $table->string('phone')->nullable();
-
+            $table->integer('role')->default(1);
             $table->rememberToken();
             $table->timestamps();
         });
