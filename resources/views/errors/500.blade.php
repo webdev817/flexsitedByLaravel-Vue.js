804e7ff9d89637027babd6d9a84ec74362edcc3d
M Awais <mawaisnow@gmail.com>
2020-03-07 21:52:26 +0500

    .....

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,5 +1,20 @@
-@extends('errors::minimal')
+@extends('layouts.welcomeWizered')
 
-@section('title', __('Server Error'))
-@section('code', '500')
-@section('message', __('Server Error'))
+@section('body')
+<div class="container mt-5 mb-10 pt-5 pb-5">
+    <div class="row justify-content-center ">
+        <div class="col-md-8 mb-2  pb-5">
+            <div class="card bg-transparent shadow">
+
+                <div class="col-xs-12 p-3 error-content text-center">
+                  <i style="font-size:26px" class="ion ion-ios-bug"></i>
+                    <h1>500</h1>
+                    <p>Oops! That's a bug</p>
+                    <span>Please send us a ticket at <a href="mailto:support@flexSited.com">support@flexSited.com</a></span>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,5 +1,31 @@
-@extends('errors::minimal')
+@extends('layouts.welcomeWizered')
 
-@section('title', __('Service Unavailable'))
-@section('code', '503')
-@section('message', __($exception->getMessage() ?: 'Service Unavailable'))
+@section('body')
+<div class="container mt-5 mb-10 pt-5 pb-5">
+
+    <div class="row mt-3">
+        <div class="col-12 text-center p-4 card border-0 shadow-sm mt-5">
+            <h1 class="text-success">
+                Planned Maintenance In Progress.
+            </h1>
+        </div>
+        <div class="col-12 card text-center pt-3 border-0 shadow-sm">
+            <p style="font-size:23px;" class="text-dark">
+                Welcome to {{ config('mawaisnow.title') }}. We are doing planned maintenance.
+                <br>
+
+                <br>
+                <strong class="mt-5 mb-5">Please check back soon.</strong>
+            </p>
+            <br>
+
+            <br>
+
+        </div>
+
+
+    </div>
+
+
+</div>
+@endsection
