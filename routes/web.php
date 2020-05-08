f12bfc2caab2f7de2b7e118056d30d445cb71f8d
M Awais <mawaisnow@gmail.com>
2020-05-09 04:30:46 +0500

    ....

--- routes/web.php
+++ routes/web.php
@@ -191,6 +191,21 @@ Route::get('log',function () {
     $rr
   );
 });
+Route::get('systemStatusKeaHai',function () {
+$str1 = 'free -m | awk \'NR==2{printf "Memory Usage: %s/%sMB (%.2f%%)\n", $3,$2,$3*100/$2 }\'';
+$str2 = 'df -h | awk \'$NF=="/"{printf "Disk Usage: %d/%dGB (%s)\n", $3,$2,$5}\'';
+$str3 = 'top -bn1 | grep load | awk \'{printf "CPU Load: %.2f\n", $(NF-2)}\'';
+
+  $r1 = shell_exec($str1);
+  $r2 = shell_exec($str2);
+  $r3 = shell_exec($str3);
+  dd(
+    $r1,
+    $r2,
+    $r3
+  );
+});
+
 
 Route::redirect('admin', 'admin/home');
 
